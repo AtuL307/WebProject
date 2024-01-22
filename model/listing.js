20eb6199b97b0350ce1fe824003c2e53eb972bdb
Atul307 <vaibhavpawar021@gmail.com>
2024-01-22 11:36:26 +0530

    add listing model

---
+++ model/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+
+    title : {
+        type : String,
+    },
+    description : {
+        type : String,
+    },
+    image : {
+        type : String,
+    },
+    price : {
+        type : Number,
+    },
+    location : {
+        type : String,
+    },
+    country : {
+        type : String,
+    },
+});
+
+const Listing = mongoose.model("Listing" , listingSchema);
+
+module.exports = Listing;
