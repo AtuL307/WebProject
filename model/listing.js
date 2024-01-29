2090dcd5cf03af05538c2a0c43884b20dbfb7493
Atul307 <vaibhavpawar021@gmail.com>
2024-01-29 18:57:54 +0530

    listing update

--- model/listing.js
+++ model/listing.js
@@ -11,6 +11,8 @@ const listingSchema = new mongoose.Schema({
     },
     image : {
         type : String,
+        set : (v) => v===" " ? "https://images.unsplash.com/photo-1706277797839-2342706023be?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+        : v,
     },
     price : {
         type : Number,
